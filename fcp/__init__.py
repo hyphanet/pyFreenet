1e140066ba720000dfc05115222bf87927034713
David McNab <david@rebirthing.co.nz>
2006-06-12 22:18:26 +0000

    now hiding nixisms if user is on doze
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/pyFreenet@9178 67a373e5-eb02-0410-a15c-ee090a768436

--- fcp/__init__.py
+++ fcp/__init__.py
@@ -1,15 +1,23 @@
+import sys, os
+
 from node import FCPNode, JobTicket
 from node import ConnectionRefused, FCPException, FCPGetFailed, \
                  FCPPutFailed, FCPProtocolError
 
 from node import SILENT, FATAL, CRITICAL, ERROR, INFO, DETAIL, DEBUG, NOISY
 
-import freenetfs
+isDoze = sys.platform.lower().startswith("win")
 
-__all__ = ['node', 'sitemgr', 'xmlrpc', 'freenetfs',
+if not isDoze:
+    import freenetfs
+
+
+__all__ = ['node', 'sitemgr', 'xmlrpc',
            'FCPNode', 'JobTicket',
            'ConnectionRefused', 'FCPException', 'FCPPutFailed',
            'FCPProtocolError',
            ]
 
+if not isDoze:
+    __all__.append('freenetfs')
 
