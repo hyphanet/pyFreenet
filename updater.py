102b7cc1bb977f4f02656e46102c61e1c3a77972
David Sowder <freenet@david.sowder.com>
2006-11-28 02:00:30 +0000

    pyfcp: The first working pyfcp updater.  Currently it should be updating the files that are needed by the latest SVN refbot.  The updater will always fetch the latest version of each file from SVN.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/pyFreenet@11080 67a373e5-eb02-0410-a15c-ee090a768436

--- updater.py
+++ updater.py
@@ -4,6 +4,11 @@ Update portions of the pyfcp files, specifically the refbot related ones at the 
 
 This is very much tied to the current way View CVS works, which we use so we
 know what SVN versions we're updating to, etc.
+
+This code was written by Zothar, November 2006, released under the GNU Lesser General
+Public License.
+
+No warranty, yada yada
 """
 
 import StringIO;
@@ -17,9 +22,6 @@ import time;
 import traceback;
 import urllib2;
 
-print "Not implemented yet (updater.py from SVN)"
-sys.exit( 1 );
-
 base_url = "http://emu.freenetproject.org/cgi-bin/viewcvs.cgi/trunk/apps/pyFreenet/";
 download_base_url = "http://emu.freenetproject.org/cgi-bin/viewcvs.cgi/*checkout*/trunk/apps/pyFreenet/";
 # updater.py should be left out of files_to_update
@@ -32,14 +34,26 @@ updater_backup_url = "https://emu.freenetproject.org/svn/trunk/apps/pyFreenet/up
 updater_filename = "updater.py";
 versions_filename = "updater_versions.dat";
 
+def do_revision_substitution( downloaded_file_lines, revision ):
+  substituted_file_lines = [];
+  substituted_string = "$Revision: %d $" % ( revision );
+  for downloaded_file_line in downloaded_file_lines:
+    substituted_file_lines.append( downloaded_file_line.replace( "$Revision$", substituted_string ));
+  return substituted_file_lines;
+
 def download_file( download_base_url, filename, version ):
   print "Downloading %s ..." % ( filename );
   download_url = "%s?rev=%d" % ( os.path.join( download_base_url, filename ), version );
-  #print "download_file(): Not implemented yet: %s" % ( download_url );
   download_url_file = urllib2.urlopen(download_url);
   download_url_lines = download_url_file.readlines()
   download_url_file.close();
   return download_url_lines;
+
+def get_remote_versions_data( url ):
+  url_file = urllib2.urlopen(url);
+  url_lines = url_file.readlines()
+  url_file.close();
+  return url_lines;
 
 def needs_update( filename, local_versions, remote_versions ):
   if( not local_versions.has_key( filename )):
@@ -49,10 +63,9 @@ def needs_update( filename, local_versions, remote_versions ):
       return True;
   return False;
 
-def process_raw_remote_versions_data( remote_versions, input_lines ):
-  print "Processing the base file list...";
+def process_raw_remote_versions_data( remote_versions, input_lines, filename_prefix ):
   for input_line in input_lines:
-    # Yeah, this is very tied to the output of View CVS at the moment...
+    # This is very tied to the output of View CVS...
     input_line = input_line.strip();
     if(0 == len( input_line )):
       continue;
@@ -61,15 +74,14 @@ def process_raw_remote_versions_data( remote_versions, input_lines ):
     i = input_line.find( searchstr );
     if( -1 == i ):
       continue;
-    #print "DEBUG: input_line: [%s]" % ( input_line );
     buf = input_line[ ( i + searchstrlen ) : ];
-    #print "  DEBUG: buf: [%s]" % ( buf );
     searchstr = '"';
     searchstrlen = len( searchstr );
     i = buf.find( searchstr );
     if( -1 == i ):
       continue;
     filename = buf[ :i ];
+    filename = filename_prefix + filename;
     searchstr = "?rev=";
     searchstrlen = len( searchstr );
     i = buf.find( searchstr );
@@ -81,7 +93,6 @@ def process_raw_remote_versions_data( remote_versions, input_lines ):
     i = buf.find( searchstr );
     if( -1 != i ):
       buf = buf[ :i ];
-    #print "  DEBUG: buf: [%s]" % ( buf );
     try:
       version = int( buf );
       remote_versions[ filename ] = version;
@@ -99,13 +110,11 @@ def read_local_versions_file( local_versions, versions_filename ):
     pass;  # Ignore a non-existent versions_file
   for versions_file_line in versions_file_lines:
     versions_file_line = versions_file_line.strip();
-    #print "DEBUG: versions_file_line: [%s]" % ( versions_file_line );
     versions_file_line_fields = versions_file_line.split();
     if( 2 != len( versions_file_line_fields )):
       continue;
     filename = versions_file_line_fields[ 1 ];
     version = versions_file_line_fields[ 0 ];
-    #print "DEBUG: filename: %s  version: %s" % ( filename, version );
     try:
       local_versions[ filename ] = int( version );
     except:
@@ -128,12 +137,13 @@ def write_local_versions_file( local_versions, versions_filename ):
 # We want to work in the directory where pyfcp is installed
 os.chdir( os.path.dirname( sys.argv[ 0 ] ));
 
-# We dont' want the updater file in files_to_update
+# We don't want the updater file in files_to_update
 if( updater_filename in files_to_update ):
   files_to_update.remove( updater_filename );
 
 local_versions = {};
 remote_versions = {};
+subdirs = [];
 read_local_versions_file( local_versions, versions_filename );
 if( not local_versions.has_key( updater_filename )):
   local_versions[ updater_filename ] = 0;
@@ -141,9 +151,7 @@ base_url_lines = [];
 updater_backup_url_lines = [];
 try:
   print "Downloading the base file list...";
-  base_url_file = urllib2.urlopen(base_url);
-  base_url_lines = base_url_file.readlines()
-  base_url_file.close();
+  base_url_lines = get_remote_versions_data( base_url );
 except:
   print "Base file list download failed.";
   try:
@@ -155,12 +163,11 @@ except:
     print "Couldn't fetch the base URL information or the updater backup URL for some reason.  You may have to update the updater via other means or try again later.";
     sys.exit( 1 );
 if( 0 != len( base_url_lines )):  # If we could download the View CVS directory listing...
-  process_raw_remote_versions_data( remote_versions, base_url_lines );
+  print "Processing the base file list...";
+  process_raw_remote_versions_data( remote_versions, base_url_lines, "" );
 else:
   print "Using the updater downloaded from the updater backup URL...";
   if( 0 != len( updater_backup_url_lines )):  # If we could download the updater from the backup URL
-    for updater_backup_url_line in updater_backup_url_lines:
-      print "DEBUG: updater_backup_url_line: [%s]" % ( updater_backup_url_line.rstrip() );
     write_file( updater_filename, updater_backup_url_lines );
     print "Executing the backup updater URL updated updater.py...";
     execfile( updater_filename );
@@ -168,16 +175,28 @@ else:
   else:
     print "Couldn't fetch the base URL information and the updater backup URL returned no data.  You may have to update the updater via other means or try again later.";
     sys.exit( 1 );
-#print "DEBUG: local_versions: [%s]" % ( local_versions );
-#print "DEBUG: remote_versions: [%s]" % ( remote_versions );
+for file_to_update in files_to_update:
+  if( -1 != file_to_update.find( "/" )):
+    file_to_update_dir = os.path.dirname( file_to_update );
+    if( not file_to_update_dir in subdirs ):
+      subdirs.append( file_to_update_dir + "/" );
+for subdir in subdirs:
+  subdir_url = base_url + subdir;
+  subdir_url_lines = [];
+  try:
+    print "Downloading the %s file list..." % ( subdir );
+    subdir_url_lines = get_remote_versions_data( subdir_url );
+  except:
+    print "%s file list download failed." % ( subdir );
+  if( 0 != len( subdir_url_lines )):  # If we could download the View CVS directory listing...
+    print "Processing the %s file list..." % ( subdir );
+    process_raw_remote_versions_data( remote_versions, subdir_url_lines, subdir );
 if( needs_update( updater_filename, local_versions, remote_versions )):
   try:
     downloaded_file_lines = download_file( download_base_url, updater_filename, remote_versions[ updater_filename ] );
   except:
     print "Couldn't fetch the updater directly.  You may have to update the updater via other means or try again later.";
     sys.exit( 1 );
-  #for downloaded_file_line in downloaded_file_lines:
-  #  print "DEBUG: downloaded_file_line: [%s]" % ( downloaded_file_line.rstrip() );
   write_file( updater_filename, downloaded_file_lines );
   local_versions[ updater_filename ] = remote_versions[ updater_filename ];
   print "Writing updated local versions data file...";
@@ -185,8 +204,18 @@ if( needs_update( updater_filename, local_versions, remote_versions )):
   print "Executing the updated updater.py...";
   execfile( updater_filename );
   sys.exit( 0 );  # execfile() doesn't appear to return, but just in case...
+updated_a_file_flag = False;
 for file_to_update in files_to_update:
   if( needs_update( file_to_update, local_versions, remote_versions )):
-    print "DEBUG: %s needs update" % ( file_to_update );
-print "Not implemented yet"
-sys.exit( 1 );
+    try:
+      downloaded_file_lines = download_file( download_base_url, file_to_update, remote_versions[ file_to_update ] );
+    except:
+      print "Couldn't fetch the updater directly.  You may have to update the updater via other means or try again later.";
+      sys.exit( 1 );
+    downloaded_file_lines = do_revision_substitution( downloaded_file_lines, remote_versions[ file_to_update ] );
+    write_file( file_to_update, downloaded_file_lines );
+    updated_a_file_flag = True;
+    local_versions[ file_to_update ] = remote_versions[ file_to_update ];
+if( updated_a_file_flag ):
+  print "Writing updated local versions data file...";
+  write_local_versions_file( local_versions, versions_filename );
