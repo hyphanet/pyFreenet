ce74b41e87521731f42e519796c638702f7c80d9
drak@kaverne <drak@kaverne>
2014-08-07 00:33:07 +0200

    Backed out changeset 4af5914452c8: did not provide better performance (it got worse).

--- fcp/pseudopythonparser.py
+++ fcp/pseudopythonparser.py
@@ -72,23 +72,22 @@ class Parser:
     
     def readline(self, line):
         """Read one line of text."""
-        # check unparsed code
-        if self.unparsed:
-            if not self.endunparsed:
-                raise ValueError("We have unparsed data but we do not know how it ends." 
-                                 "THIS IS A BUG.")
-            else:
-                self.unparsed += "\n" + line
-                # if the line ends the unparsed code, store it.
-                if line.strip().endswith(self.endunparsed):
-                    # json uses null for None, true for True and false for False. 
-                    # We have to replace those in the content and hope that nothing will break.
-                    self.data[self.unparsedvariable] = self.jsonload(self.unparsed)
-                    self.unparsed, self.endunparsed = "", ""
-                # if the line does not end the unparsed code, just
-                # keep the code we added to the unparsed code
-                return
-                
+        # if we have unparsed code and this line does not end it, we just add the code to the unparsed code.
+        if self.unparsed and not self.endunparsed:
+            raise ValueError("We have unparsed data but we do not know how it ends. THIS IS A BUG.")
+        
+        if self.unparsed and self.endunparsed:
+            self.unparsed += "\n" + line
+        
+        if self.unparsed and self.endunparsed and not line.strip().endswith(self.endunparsed):
+            return # line is already processed as far as possible
+        if self.unparsed and self.endunparsed and line.strip().endswith(self.endunparsed):
+            # json uses null for None, true for True and false for False. 
+            # We have to replace those in the content and hope that nothing will break.
+            data = self.jsonload(self.unparsed)
+            self.data[self.unparsedvariable] = data
+            self.unparsed, self.endunparsed = "", ""
+            return
         
         # start reading complex datastructures
         if " = [" in line:
@@ -98,7 +97,7 @@ class Parser:
             self.endunparsed = "]"
             self.checkandprocesslinerest(self.unparsed)
             return
-        if " = {" in line:
+        elif " = {" in line:
             start = line.index(" = {")
             self.unparsedvariable = line[:start]
             self.unparsed = line[start+3:]
@@ -107,17 +106,19 @@ class Parser:
             return
         
         # handle the easy cases
-        if (not line.strip()                # ignore empty lines
-            or line.strip().startswith("#") # ignore comments
-            or not " = " in line):          # the only thing left to
-                                            # care for are variable
-                                            # assignments
+        # ignore empty lines
+        if not line.strip():
+            return
+        # ignore comments
+        if line.strip().startswith("#"):
+            return
+        # the only thing left to care for are variable assignments
+        if not " = " in line:
             return
         
         # prepare reading variables
         start = line.index(" = ")
         variable = line[:start]
-        # TODO: use a pre-created regexp for higher speed.
         forbiddenvariablechars = " ", ".", "+", "-", "=", "*", "/"
         if True in [i in variable for i in forbiddenvariablechars]:
             raise ValueError("Variables must not contain any of the forbidden characters: " + str(forbiddenvariablechars))
