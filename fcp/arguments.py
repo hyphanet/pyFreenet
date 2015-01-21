5fef46d91941ce7ed8569e707786528e00ba6c9d
Steve Dougherty <steve@asksteved.com>
2015-01-20 22:56:33 -0500

    Break long lines

--- fcp/arguments.py
+++ fcp/arguments.py
@@ -19,7 +19,21 @@ def add_default_arguments(parser):
     '''
     Add the default arguments to an argparse parser.
     '''
-    parser.add_argument('--version', '-V', action='version', version="%(prog)s " + fcp.node.fcpVersion)
-    parser.add_argument('--fcphost', '-H', default=fcp.node.defaultFCPHost, help='Connect to FCP service at host <FCPHOST>. (You may also use the environment variable FCP_HOST)')
-    parser.add_argument('--fcpport', '-P', default=fcp.node.defaultFCPPort, type=int, help='Connect to FCP service at port <FCPPORT>. (You may also use the environment variable FCP_PORT)')
-    parser.add_argument('--timeout', '-t', default=fcp.node.ONE_YEAR, type=timeout_type, help='Set the timeout, in seconds, for completion. Default one year')
+    parser.add_argument('--version', '-V', action='version',
+                        version="%(prog)s " + fcp.node.fcpVersion)
+    parser.add_argument('--fcphost', '-H', default=fcp.node.defaultFCPHost,
+                        help="""Connect to FCP service at host <FCPHOST>.
+                             (You may also use the environment variable
+                              FCP_HOST.)
+                             """)
+    parser.add_argument('--fcpport', '-P', default=fcp.node.defaultFCPPort,
+                        type=int,
+                        help="""Connect to FCP service at port <FCPPORT>.
+                             (You may also use the environment variable
+                              FCP_PORT.)
+                             """)
+    parser.add_argument('--timeout', '-t', default=fcp.node.ONE_YEAR,
+                        type=timeout_type,
+                        help="""Set the timeout, in seconds, for completion.
+                             Default one year.
+                             """)
