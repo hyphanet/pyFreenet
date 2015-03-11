5949d959a341dc50f731ca825e93bb5dc96af80c
Marios Isaakidis <misaakidis@yahoo.gr>
2015-03-11 14:34:36 +0100

    Check if the given URI at loadplugin is valid

--- fcp/loadplugin.py
+++ fcp/loadplugin.py
@@ -9,6 +9,8 @@ This is the guts of the command-line front-end app fcploadplugin
 import argparse
 import sys
 import traceback
+import os.path
+from urlparse import urlparse
 
 from . import node
 from .arguments import add_default_arguments
@@ -48,17 +50,33 @@ def main(argv=sys.argv[1:]):
     Front end for fcploadplugin utility
     """
 
-    parser = create_parser()
-    args = parser.parse_args()
-
-    plugin_uri = args.plugin_uri
-
     # default job options
     verbosity = node.ERROR
     verbose = False
 
     parser = create_parser()
     args = parser.parse_args(argv)
+    uri = args.plugin_uri
+
+    keytypes = ["USK", "KSK", "SSK", "CHK"]
+
+    if os.path.isfile(uri) \
+    or os.path.islink(uri) and os.path.isfile(os.path.realpath(uri)):
+        if uri.endswith(".jar"):
+            plugin_uri = uri
+    elif urlparse(uri).scheme != "":
+        plugin_uri = uri
+    else:
+        if not uri.startswith("freenet:"):
+            uri = "freenet:" + uri
+        if uri[len("freenet:"):len("freenet:")+3] in keytypes:
+            plugin_uri = uri
+
+    try:
+        plugin_uri
+    except NameError:
+        sys.stderr.write("The given plugin uri is not valid.\n")
+        sys.exit(1)
 
     # try to create the node
     try:
@@ -68,7 +86,7 @@ def main(argv=sys.argv[1:]):
         if verbose:
             traceback.print_exc(file=sys.stderr)
         sys.stderr.write("Failed to connect to FCP service at %s:%d\n" % (args.fcphost, args.fcpport))
-        sys.exit(1)
+        sys.exit(2)
 
     # send the LoadPlugin request
     fcp_node.fcpLoadPlugin(plugin_uri)
