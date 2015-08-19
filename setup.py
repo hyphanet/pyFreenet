e0edc599b899d02d49f326d93e75b7fcbc7f3ece
Arne Babenhauserheide <bab@draketo.de>
2015-08-19 23:04:02 +0200

    version 0.3.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class pyfreenet_install(distutils.command.install.install):
 
 
 setup(name="pyFreenet",
-      version="0.3.1",
+      version="0.3.2",
       description="Freenet Client Protocol Helper",
       author="Arne Babenhauserheide",
       author_email="arne_bab@web.de",
