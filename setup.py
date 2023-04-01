f46f1def6b358f9a47bbbc1b6b7ceaeb5819b7db
Arne Babenhauserheide <arne_bab@web.de>
2023-04-01 11:11:18 +0200

    release 0.5.8

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class pyfreenet_install(distutils.command.install.install):
 
 
 setup(name="pyFreenet3",
-      version="0.5.7",
+      version="0.5.8",
       description="Freenet Client Protocol Helper",
       author="Arne Babenhauserheide",
       author_email="arne_bab@web.de",
