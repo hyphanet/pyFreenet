b177e8e6cb80572852227b7341756eff7cfb3d2b
Arne Babenhauserheide <arne_bab@web.de>
2023-11-16 13:04:14 +0100

    Increase version to 0.6.1 and update Changelog

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class pyfreenet_install(distutils.command.install.install):
 
 
 setup(name="pyFreenet3",
-      version="0.6.0",
+      version="0.6.1",
       description="Freenet Client Protocol Helper",
       author="Arne Babenhauserheide",
       author_email="arne_bab@web.de",
