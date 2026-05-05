42b64024274cce729f04943e67bb4cfb2b757550
Debora Wöpcke <deboradub@gmail.com>
2026-05-05 23:25:09 +0200

    Add async variant of some functions where it is useful

---
+++ fcp3/anode.py
@@ -0,0 +1,151 @@
+"""
+An implementation of a freenet client library for
+FCP v2, offering considerable flexibility.
+
+Clients should instantiate ANode, then await its methods to perform
+tasks with FCP.
+
+This module is prepared for use with the asyncio, async and await
+constructs in python3.
+
+It is a wrapper around the FCPNode and other functions in node.py.
+
+This was written 2026 and released under the GNU Lesser General Public License.
+"""
+
+import asyncio
+import logging
+from typing import Any, Self
+
+from .node import FCPNode
+
+
+def get_future() -> asyncio.Future:
+    return asyncio.get_running_loop().create_future()
+
+
+class ANode:
+    """
+    Represents an interface to freenet node via its FCP port,
+    and exposes primitives for operations in FCP.
+
+    All operations are async operations (awaitable coroutines) to work
+    with the python3 asyncio library.
+    """
+
+    def __init__(self, **kw) -> None:
+        self.kw = kw
+
+    async def start(self) -> None:
+        # TODO: Real async operation currently missing
+        # The real operation should not block on socket operations.
+        # Fixing this is not a high priority since this method is
+        # probably only called once anyway.
+        self.node = FCPNode(**self.kw)
+        self.node.noCloseSocket = False
+
+    async def __aenter__(self) -> Self:
+        await self.start()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        await self.shutdown()
+
+    async def shutdown(self) -> None:
+        if hasattr(self, 'node'):
+            self.node.shutdown()
+
+    class CallbackException(Exception):
+        """
+        Unexpected message in the interaction to FCPNode.
+        """
+
+    class Callback(object):
+
+        def __init__(self, fut: asyncio.Future) -> None:
+            self.fut = fut
+            self.loop = asyncio.get_running_loop()
+
+        def _set_result(self, result: Any) -> None:
+            self.loop.call_soon_threadsafe(self.fut.set_result, result)
+
+        def _set_exception(self, exception: Any) -> None:
+            self.loop.call_soon_threadsafe(self.fut.set_exception, exception)
+
+        def __call__(self, status: str, result: Any) -> Any:
+            if status == 'successful':
+                self._set_result(result)
+            elif status == 'pending':
+                pass
+            elif status == 'failed':
+                self._set_exception(result)
+            else:
+                self._set_exception(ANode.CallbackException(
+                    f'Unknown status {status}'))
+
+        def get_result(self) -> Any:
+            return self.fut.result()
+
+    def _asyncify(f):
+        async def wrap(*args, **kw):
+            assert 'async' not in kw
+            assert 'callback' not in kw
+
+            logging.debug('Starting %s %s %s', f.__name__, args, kw)
+            kw['async'] = True
+
+            res = get_future()
+            kw['callback'] = ANode.Callback(res)
+            f(*args, **kw)
+            await res
+            logging.debug('Done %s %s %s %s', f.__name__, args, kw, res)
+            return res.result()
+        return wrap
+
+    @_asyncify
+    def get(self, *args, **kwargs) -> Any:
+        self.node.get(*args, **kwargs)
+
+    @_asyncify
+    def put(self, *args, **kwargs) -> Any:
+        self.node.put(*args, **kwargs)
+
+    class Callback2(Callback):
+        def __init__(self,
+                     res: asyncio.Future,
+                     key_fut: asyncio.Future) -> None:
+            self.key_fut = key_fut
+            super().__init__(res)
+
+        def __set_key_result(self, result: Any) -> None:
+            self.loop.call_soon_threadsafe(self.key_fut.set_result, result)
+
+        def __call__(self, status: str, result: Any) -> Any:
+            if status == 'pending':
+                self.__set_key_result(result['URI'])
+            else:
+                return ANode.Callback.__call__(self, status, result)
+
+        async def get_a_result(self) -> Any:
+            await self.fut
+            return self.fut.result()
+
+    async def put2(self, taskgroup: asyncio.TaskGroup, *args, **kwargs) -> str:
+        """Put operation that returns the key immediately when queued.
+        It puts the rest of the operation into the given task group
+        to be waited for afterwards.
+        """
+        assert 'async' not in kwargs
+        assert 'callback' not in kwargs
+
+        logging.debug('Starting put2 %s %s', args, kwargs)
+        kwargs['async'] = True
+
+        res = get_future()
+        keyres = get_future()
+        kwargs['callback'] = self.Callback2(res, keyres)
+        self.node.put(*args, **kwargs)
+        await keyres
+        taskgroup.create_task(kwargs['callback'].get_a_result())
+        logging.debug('Queued put2 %s %s %s', args, kwargs, res)
+        return keyres.result()
---
+++ test_anode.py
@@ -0,0 +1,156 @@
+"""
+Testing fcp3.anode against a real node.
+"""
+
+import asyncio
+import logging
+import random
+import time
+import unittest
+import fcp3.anode
+
+
+class SmokeTest(unittest.IsolatedAsyncioTestCase):
+    uri1 = (
+        "USK@E0jWjfYUfJqESuiM~5ZklhTZXKCWapxl~CRj1jmZ-~I," +
+        "gl48QSprqZC1mASLbE9EOhQoBa~PheO8r-q9Lqj~uXA,AQACAAE/index.yml/2497"
+    )
+
+    async def test_simple_get(self) -> None:
+        node = fcp3.anode.ANode()
+        try:
+            await node.start()
+            mimetype, data, details = \
+                await node.get(self.uri1, realtime=True, priority=0)
+        finally:
+            await node.shutdown()
+        self.assertEqual(mimetype, 'application/octet-stream')
+        self.assertTrue(isinstance(data, bytearray))
+
+    async def test_with_node(self) -> None:
+        async with fcp3.anode.ANode() as node:
+            mimetype, data, details = \
+                await node.get(self.uri1, realtime=True, priority=0)
+            self.assertEqual(mimetype, 'application/octet-stream')
+            self.assertTrue(isinstance(data, bytearray))
+
+
+class TestParallel(unittest.IsolatedAsyncioTestCase):
+    uri_root = (
+        "USK@E0jWjfYUfJqESuiM~5ZklhTZXKCWapxl~CRj1jmZ-~I," +
+        "gl48QSprqZC1mASLbE9EOhQoBa~PheO8r-q9Lqj~uXA,AQACAAE/index.yml/"
+    )
+    DATA = b'some data'
+    DATA_KEY = 'CHK@Lw5APCokPPd8SQWxX1V87NwFSIcEGeqYvKnbwgpjIj8'
+
+    def setUp(self) -> None:
+        self.anode = fcp3.anode.ANode()  # (verbosity=fcp3.node.DEBUG)
+
+    async def asyncSetUp(self) -> None:
+        await self.anode.start()
+
+    async def asyncTearDown(self) -> None:
+        await self.anode.shutdown()
+
+    def tearDown(self) -> None:
+        self.anode = None
+
+    async def test_one_get(self) -> None:
+        mimetype, data, details = \
+            await self.anode.get(self.uri_root + "24000",
+                                 realtime=True,
+                                 priority=0)
+        self.assertEqual(mimetype, 'application/octet-stream')
+        self.assertTrue(isinstance(data, bytearray))
+
+    async def test_many_gets(self) -> None:
+        array = await asyncio.gather(
+            *[self.anode.get(self.uri_root + str(i),
+                             priority=i % 7)
+              for i in range(30)])
+        self.assertEqual(len(array), 30)
+
+    async def test_one_put(self) -> None:
+        res = await self.anode.put(data=self.DATA)
+        self.assertEqual(res[0:10], self.DATA_KEY[0:10])
+
+    async def test_one_put2(self) -> None:
+        async with asyncio.TaskGroup() as tg:
+            res = await self.anode.put2(tg, data=self.DATA)
+            self.assertEqual(res[0:10], self.DATA_KEY[0:10])
+
+    async def test_many_put(self) -> None:
+        SIZE = 5
+        array = await asyncio.gather(
+            *[self.anode.put(data=self.DATA + bytes(str(i), 'utf-8'),
+                             priority=i % 7)
+              for i in range(SIZE)])
+        self.assertEqual(len(array), SIZE)
+        for key in array:
+            self.assertTrue(isinstance(key, str))
+            self.assertEqual(key[0:4], 'CHK@')
+
+    async def create_node_with_leafs(self, size: int) -> str:
+        if size == 0:
+            return 'leaf ' + str(random.randint(0, 10000))
+        await asyncio.sleep(1)
+        return await self.anode.put(
+            data=bytes("size " + str(size) + "\n" +
+                       "\n".join(await asyncio.gather(
+                           *[self.create_node_with_leafs(
+                               random.randint(0, size - 1))
+                             for i in range(size)])), 'utf-8'))
+
+    async def check_tree(self, uri: str) -> None:
+        mimetype, data, details = \
+            await self.anode.get(uri)
+        async with asyncio.TaskGroup() as tg:
+            for line in str(data, 'utf-8').split('\n'):
+                if line.startswith('size '):
+                    continue
+                if line.startswith('leaf '):
+                    continue
+                tg.create_task(self.check_tree(line))
+
+    async def run_massive_test_for_build_and_check_trees(self) -> None:
+        for s in range(1, 30):
+            start_time = time.time()
+            key = await self.create_node_with_leafs(s)
+            print(key)
+            await self.check_tree(key)
+            elapsed_time = time.time() - start_time
+            print(f'Tree with max-depth {s} took {elapsed_time:.1f}s',
+                  'to create and retrieve')
+
+    async def create_node_with_leafs2(self,
+                                      size: int,
+                                      taskgroup: asyncio.TaskGroup) -> str:
+        if size == 0:
+            return 'leaf ' + str(random.randint(0, 10000))
+        await asyncio.sleep(1)
+        return await self.anode.put2(
+            taskgroup,
+            data=bytes("size " + str(size) + "\n" +
+                       "\n".join(await asyncio.gather(
+                           *[self.create_node_with_leafs2(
+                               random.randint(0, size - 1), taskgroup)
+                             for i in range(size)])), 'utf-8'))
+
+    async def run_massive_test_for_build_and_check_trees2(self) -> None:
+        for s in range(1, 30):
+            start_time = time.time()
+            async with asyncio.TaskGroup() as tg:
+                key = await self.create_node_with_leafs2(s, tg)
+                print(key)
+                until_waiting = time.time() - start_time
+                print(f'Waiting took {until_waiting:.1f}s to queue')
+            await self.check_tree(key)
+            elapsed_time = time.time() - start_time
+            print(f'Tree with max-depth {s} took {elapsed_time:.1f}s',
+                  'to create and retrieve')
+
+
+if __name__ == '__main__':
+    log = logging.getLogger('root')
+    # log.setLevel(logging.DEBUG)
+    unittest.main()
